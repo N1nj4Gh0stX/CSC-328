2d3100331b0e0f733b83a85d84d28670573b4580
lizulla <lizmarydelarosa77@gmail.com>
2024-11-19 09:05:27 -0500

    Update socket.cpp

--- socket.cpp
+++ socket.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdint.h>
 
+#include "socket.h"
+
+mysock::mysock(){
+  
+}
