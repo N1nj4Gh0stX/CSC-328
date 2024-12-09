071e030847929b8a123d20f5d6d825ac4101d54a
lizulla <lizmarydelarosa77@gmail.com>
2024-12-09 11:03:13 -0500

    Create clientparse.cpp

---
+++ clientparse.cpp
@@ -0,0 +1,29 @@
+//liz parsing.cpp
+#include <iostream>
+#include <unistd.h>
+#include "parse.h"
+
+
+using namespace std;
+
+struct options parsemenu(int argc, char* argv[]){
+	struct options o;
+	o.hostname = "";
+	o.port = "";
+	int opt;
+	while((opt = getopt(argc, argv, "h:p:")) != -1){
+		switch (opt){
+			case 'h':
+				o.hostname = optarg;
+				// cout << "hostname: " << o.hostname << endl;
+				break;
+			
+			case 'p':
+				o.port = optarg;
+				// cout << "port: " << o.port << endl;
+				break;
+			
+		}
+	}
+	return o;
+}
