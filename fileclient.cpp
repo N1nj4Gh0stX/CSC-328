82f0f91bea614395d277b311c8e5a72ea3be6cbf
lizulla <lizmarydelarosa77@gmail.com>
2024-12-09 11:02:32 -0500

    Update fileclient.cpp

--- fileclient.cpp
+++ fileclient.cpp
@@ -300,33 +300,3 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
-//liz parsing.cpp?
-#include <iostream>
-#include <unistd.h>
-#include "parse.h"
-
-
-using namespace std;
-
-struct options parsemenu(int argc, char* argv[]){
-	struct options o;
-	o.hostname = "";
-	o.port = "";
-	int opt;
-	while((opt = getopt(argc, argv, "h:p:")) != -1){
-		switch (opt){
-			case 'h':
-				o.hostname = optarg;
-				// cout << "hostname: " << o.hostname << endl;
-				break;
-			
-			case 'p':
-				o.port = optarg;
-				// cout << "port: " << o.port << endl;
-				break;
-			
-		}
-	}
-	return o;
-}
