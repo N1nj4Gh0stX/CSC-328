09b5d90fdd38dcc64c96131500e4462fa8bd7aec
lizulla <lizmarydelarosa77@gmail.com>
2024-12-09 13:04:18 -0500

    Create clientparse.h

---
+++ clientparse.h
@@ -0,0 +1,31 @@
+/*************************************************************/
+/* Author: Lizmary Delarosa
+/* Filename: parse.h
+/* Purpose: This header file declares the parsemenu function, 
+/*	    responsible for parsing command-line.
+/*	    It also includes the struct options variables that will 
+/*	    be declared in parse.c.
+/************************************************************/
+#ifndef PARSE_H
+#define PARSE_H
+
+#include <string>
+using namespace std;
+
+struct options{
+	string hostname;
+	string port;
+};
+/*************************************************************************/
+/* Function name: parsemenu */
+/* Description: Parses command-line arguments to determine the port,
+/*		the directory, and the call to verbose. Port is required by 
+/* 		the user, while the directory and verbose have default
+/*		values.
+/* Parameters: int argc - The number of command-line arguments 
+/*             char* argv[] - Array of command-line arguments 
+/* Return Value: struct options
+/*************************************************************************/
+struct options parsemenu(int argc, char* argv[]);
+
+#endif 
